97a225d21054ddca9cc8f6e85663fa0ef78c473e
Gregory H. Halverson <gregory.h.halverson@jpl.nasa.gov>
2025-09-30 11:09:02 -0700

    water surface energy balance

--- AquaSEBS/AquaSEBS.py
+++ AquaSEBS/AquaSEBS.py
@@ -1,46 +1,93 @@
+from typing import Union
+import numpy as np
+from datetime import datetime
+import rasters as rt
+from rasters import Raster, RasterGeometry
+from GEOS5FP import GEOS5FP
+from check_distribution import check_distribution
+from priestley_taylor import epsilon_from_Ta_C, GAMMA_PA
+from verma_net_radiation import verma_net_radiation
+
+from .constants import *
+
 from .water_heat_flux import water_heat_flux
 
-def AquaSEBS():
-    # dew-point temperature in Celsius
-    Td_C = Ta_C - ((100 - RH * 100) / 5.0)
-    self.diagnostic(Td_C, "Td_C", date_UTC, target)
+W_MAX_PROPORTION = 0.5  # Maximum proportion of net radiation that can be used for water heat flux
+PT_ALPHA = 1.26  # Priestley-Taylor coefficient
 
-    if wind_speed is None:
-        wind_speed = self.wind_speed(time_UTC=time_UTC, geometry=geometry)
+## TODO use NASADEM surface water body extent to mask out land when processing on rasters
 
-    # water heat flux
-    W = water_heat_flux(
-        ST_C=ST_C,
-        Td_C=Td_C,
-        windspeed_mps=wind_speed,
-        SWnet=SWnet
+def AquaSEBS(
+        WST_C: Union[Raster, np.ndarray],
+        emissivity: Union[Raster, np.ndarray] = None,
+        albedo: Union[Raster, np.ndarray] = None,
+        Ta_C: Union[Raster, np.ndarray] = None,
+        RH: Union[Raster, np.ndarray] = None,
+        Td_C: Union[Raster, np.ndarray] = None,
+        windspeed_mps: Union[Raster, np.ndarray] = None,
+        SWnet: Union[Raster, np.ndarray] = None,
+        Rn_Wm2: Union[Raster, np.ndarray] = None,
+        W_Wm2: Union[Raster, np.ndarray] = None,
+        SWin_Wm2: Union[Raster, np.ndarray] = None,
+        geometry: RasterGeometry = None,
+        time_UTC: datetime = None,
+        GEOS5FP_connection: GEOS5FP = None,
+        gamma_Pa: Union[Raster, np.ndarray, float] = GAMMA_PA,
+        resampling: str = RESAMPLING_METHOD):
+    # Compute net radiation if not provided, using albedo, ST_C, and emissivity
+    if Rn_Wm2 is None and albedo is not None and WST_C is not None and emissivity is not None:
+        # Retrieve incoming shortwave if not provided
+        if SWin_Wm2 is None and geometry is not None and time_UTC is not None:
+            SWin_Wm2 = GEOS5FP_connection.SWin(
+                time_UTC=time_UTC,
+                geometry=geometry,
+                resampling=resampling
+            )
+
+        # Calculate net radiation using Verma et al. method
+        Rn_results = verma_net_radiation(
+            SWin_Wm2=SWin_Wm2,
+            albedo=albedo,
+            ST_C=ST_C,
+            emissivity=emissivity,
+            Ta_C=Ta_C,
+            RH=RH,
+            geometry=geometry,
+            time_UTC=time_UTC,
+            resampling=resampling,
+            GEOS5FP_connection=GEOS5FP_connection
+        )
+
+        Rn_Wm2 = Rn_results["Rn_Wm2"]
+
+    if Rn_Wm2 is None:
+        raise ValueError("net radiation (Rn_Wm2) not given")
+
+    if W_Wm2 is None:
+        # water heat flux
+        W_Wm2 = water_heat_flux(
+            WST_C=WST_C,
+            Ta_C=Ta_C,
+            Td_C=Td_C,
+            windspeed_mps=windspeed_mps,
+            SWnet=SWnet,
+            geometry=geometry,
+            time_UTC=time_UTC,
+            GEOS5FP_connection=GEOS5FP_connection,
+            resampling=resampling
+        )
+
+        W_Wm2 = rt.clip(W_Wm2, 0, W_MAX_PROPORTION * Rn_Wm2)
+    
+    check_distribution(W_Wm2, "W_Wm2")
+
+    epsilon = epsilon_from_Ta_C(
+        Ta_C=Ta_C,
+        gamma_Pa=gamma_Pa
     )
 
-    W = rt.clip(W, 0, W_MAX_PROPORTION * Rn)
-    W = W.mask(water)
-    self.diagnostic(W, "W", date_UTC, target, blank_OK=True)
+    LE_Wm2 = PT_ALPHA * epsilon * (Rn_Wm2 - W_Wm2)
+    check_distribution(LE_Wm2, "LE_Wm2")
 
-    # calculate soil evaporation (LEs) from relative surface wetness, soil moisture constraint,
-    # priestley taylor coefficient, epsilon = delta / (delta + gamma), net radiation of the soil,
-    # and soil heat flux
-    LE_soil = rt.clip((fwet + fREW * (1 - fwet)) * PT_ALPHA * epsilon * (Rn_soil - G), 0, None)
-    self.diagnostic(LE_soil, "LE_soil", date_UTC, target)
+    return LE_Wm2
 
-    # canopy transpiration
-
-    # calculate net radiation of the canopy from net radiation of the soil
-    Rn_canopy = Rn - Rn_soil
-    self.diagnostic(Rn_canopy, "Rn_canopy", date_UTC, target)
-
-    # calculate potential evapotranspiration (pET) from net radiation, and soil heat flux
-
-    PET_water = PT_ALPHA * epsilon * (Rn - W)
-    self.diagnostic(PET_water, "PET_water", date_UTC, target, blank_OK=True)
-    PET_land = PT_ALPHA * epsilon * (Rn - G)
-    self.diagnostic(PET_land, "PET_land", date_UTC, target)
-
-    PET = rt.where(
-        water,
-        PET_water,
-        PET_land
-    )
